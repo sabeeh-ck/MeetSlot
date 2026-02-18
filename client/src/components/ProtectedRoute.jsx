ffb86da898a9540ad5033a0a750819c7f2b4b9ca
sabeeh <sabeehck02@gmail.com>
2026-02-18 23:49:12 +0530

    refactor ProtectedRoute to use user authentication instead of token

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -2,9 +2,11 @@ import { Navigate } from "react-router";
 import { useAuth } from "../context/AuthContext";
 
 const ProtectedRoute = ({ children }) => {
-    const { token } = useAuth();
+    const { user, loading } = useAuth();
 
-    if (!token) return <Navigate to={"/login"} replace />;
+    if (loading) return <div>Loading...</div>;
+
+    if (!user) return <Navigate to={"/login"} replace />;
 
     return children;
 };

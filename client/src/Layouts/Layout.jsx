108f77d0dd4704a612bf8943a7418f218d711a1f
sabeeh <sabeehck02@gmail.com>
2026-04-14 23:35:01 +0530

    refactor: implement Layout component and remove unused Footer component

--- client/src/Layouts/Layout.jsx
+++ client/src/Layouts/Layout.jsx
@@ -1,12 +1,10 @@
 import { Outlet } from "react-router";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Header from "../components/Header";
 
 const Layout = () => (
     <>
         <Header />
         <Outlet />
-        <Footer />
     </>
 );
 
--- client/src/components/Footer.jsx
+++
@@ -1,5 +0,0 @@
-const Footer = () => {
-    return <footer className="flex"></footer>;
-};
-
-export default Footer;

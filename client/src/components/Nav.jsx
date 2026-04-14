ffa57912658d2d443aff4a323768aaa060d366be
sabeeh <sabeehck02@gmail.com>
2026-04-15 00:59:24 +0530

    refactor: streamline Nav component by consolidating icon imports and enhancing navigation structure

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,108 +1,113 @@
-import { NavLink, useLocation, useNavigate } from "react-router";
-import { HomeIcon, LogoutIcon, MenuIcon, UserIconOutline } from "../icons";
+import { NavLink } from "react-router";
+import {
+    CalendarIconOutline as BookingsIconOutline,
+    CalendarIconSolid as BookingsIconSolid,
+    CogIconOutline,
+    CogIconSolid,
+    DashboardIconOutline,
+    DashboardIconSolid,
+    EmployeesOutline,
+    EmployeesSolid,
+    HomeIconOutline,
+    HomeIconSolid,
+    RoomIconOutline,
+    RoomIconSolid,
+    UserIconOutline,
+    UserIconSolid,
+} from "../icons";
 import { useAuth } from "../context/AuthContext";
 
-const Nav = ({ menu }) => {
-    const { logout, user } = useAuth();
-    const navigate = useNavigate();
+const NAV_ITEMS = [
+    {
+        name: "Dashboard",
+        outline: DashboardIconOutline,
+        solid: DashboardIconSolid,
+        path: "./dashboard",
+        roles: ["admin"],
+    },
+    {
+        name: "Bookings",
+        outline: BookingsIconOutline,
+        solid: BookingsIconSolid,
+        path: "./bookings",
+        roles: ["admin"],
+    },
+    {
+        name: "Rooms",
+        outline: RoomIconOutline,
+        solid: RoomIconSolid,
+        path: "./rooms",
+        roles: ["admin"],
+    },
+    {
+        name: "Home",
+        outline: HomeIconOutline,
+        solid: HomeIconSolid,
+        path: "./home",
+        roles: ["employee"],
+    },
+    {
+        name: "Meetings",
+        outline: UserIconOutline,
+        solid: UserIconSolid,
+        path: "./meetings",
+        roles: ["employee"],
+    },
+    {
+        name: "Employees",
+        outline: EmployeesOutline,
+        solid: EmployeesSolid,
+        path: "./employees",
+        roles: ["admin"],
+    },
+    {
+        name: "Settings",
+        outline: CogIconOutline,
+        solid: CogIconSolid,
+        path: "./settings",
+        roles: ["employee"],
+    },
+];
 
-    const { pathname } = useLocation();
-
-    const home = pathname === "/";
-
-    const navItems = [
-        {
-            name: "Dashboard",
-            icon: "",
-        },
-        {
-            name: "Bookings",
-            icon: "",
-        },
-        {
-            name: "Rooms",
-            icon: "",
-        },
-        {
-            name: "My Meetings",
-            icon: <UserIconOutline className="h-5" />,
-        },
-    ];
+const Nav = () => {
+    const { user } = useAuth();
 
     return (
-        <>
-            {user?.role === "admin" ? (
-                <nav className="z-60 hidden h-full items-start justify-between gap-2 font-semibold lg:flex lg:flex-col">
-                    <div className="flex w-full flex-col gap-2">
-                        {navItems.map(({ name, icon }) => (
-                            <button
-                                key={name}
-                                className="active:bg-border border-border md:hover:bg-border flex w-full items-center gap-2 rounded-lg p-2"
-                            >
-                                {icon}
-                                <p className="select-none">{name}</p>
-                            </button>
-                        ))}
-                    </div>
-
-                    <div className="flex w-full flex-col items-center gap-4">
-                        <button className="active:bg-textmute md:hover:bg-textmute bg-text text-bg flex w-40 justify-center gap-2 rounded-lg p-2">
-                            New Meeting
-                        </button>
-
-                        <button
-                            className="active:bg-border md:hover:bg-border border-border flex w-40 items-center justify-center gap-2 rounded-lg border p-2"
-                            onClick={() => {
-                                logout();
-                                navigate("/login");
-                            }}
-                        >
-                            <LogoutIcon className="h-5" />
-                            <p className="text-base select-none">Log Out</p>
-                        </button>
-                    </div>
-                </nav>
-            ) : (
-                <nav className="z-60 hidden gap-4 font-semibold lg:flex">
-                    {home ? (
+        <nav className="z-60 flex h-full items-start justify-between gap-2 font-semibold lg:flex-col">
+            <div className="flex w-full gap-2 lg:flex-col">
+                {NAV_ITEMS.filter(({ roles }) =>
+                    roles.includes(user?.role),
+                ).map(({ name, path, solid, outline }) => {
+                    return (
                         <NavLink
-                            to="/user"
-                            className={
-                                "active:bg-border border-border md:hover:bg-border flex items-center gap-2 rounded-lg border p-2"
+                            key={name}
+                            to={path}
+                            className={({ isActive }) =>
+                                `border-border active:bg-border md:hover:bg-border w-full gap-1 rounded-lg p-2 ${
+                                    isActive ? "bg-border" : "bg-none"
+                                } `
                             }
                         >
-                            <UserIconOutline className="h-5" />
-                            <p className="select-none">My Meetings</p>
-                        </NavLink>
-                    ) : (
-                        <NavLink
-                            to="/"
-                            className="active:bg-border border-border md:hover:bg-border flex items-center gap-2 rounded-lg border p-2"
-                        >
-                            <HomeIcon className="h-5" />
-                            <p className="select-none">Home</p>
-                        </NavLink>
-                    )}
+                            {({ isActive }) => {
+                                const Icon = isActive ? solid : outline;
 
-                    <button
-                        className="active:bg-border md:hover:bg-border border-border flex items-center gap-2 rounded-lg border p-2"
-                        onClick={() => {
-                            logout();
-                            navigate("/login");
-                        }}
-                    >
-                        <LogoutIcon className="h-5" />
-                        <p className="text-base select-none">Log Out</p>
-                    </button>
-                </nav>
-            )}
-
-            <button
-                className={`border-border active:bg-border rounded-lg border p-2 lg:hidden ${menu ? "bg-border" : "bg-surface"}`}
-            >
-                <MenuIcon className="h-5" />
-            </button>
-        </>
+                                return (
+                                    <div className="flex flex-col items-center lg:flex-row lg:gap-2">
+                                        <Icon className="h-5 w-5" />
+                                        <span className="text-xs select-none lg:text-sm">
+                                            {name}
+                                        </span>
+                                        {isActive && (
+                                            <div className="bg-text ml-auto hidden h-5 w-1.5 rounded-sm lg:block" />
+                                        )}
+                                    </div>
+                                );
+                            }}
+                        </NavLink>
+                    );
+                })}
+            </div>
+        </nav>
     );
 };
 

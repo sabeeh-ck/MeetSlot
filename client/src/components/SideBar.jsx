966279253aeed7a667f737177fdb9a90c5315134
sabeeh <sabeehck02@gmail.com>
2026-04-15 15:06:26 +0530

    refactor: enhance SideBar component with admin role navigation and add logout button

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,16 +1,33 @@
-import { ChevronDoubleLeftIcon, PlusIcon } from "../icons";
+import { useNavigate } from "react-router";
+import { useAuth } from "../context/AuthContext";
+import { ChevronDoubleLeftIcon, LogoutIcon, PlusIcon } from "../icons";
 import Nav from "./Nav";
 
 const SideBar = ({ activeTab, setSearchParams }) => {
+    const { user } = useAuth();
+    const navigate = useNavigate();
+
     return (
-        <div className="flex h-full w-full flex-col justify-between">
-            <div className="flex h-full flex-col justify-between p-4">
+        <div className="flex h-full flex-col justify-between">
+            <div className="flex h-full flex-col p-4">
                 <Nav activeTab={activeTab} setSearchParams={setSearchParams} />
 
-                <div className="mb-10 flex w-full flex-col items-center gap-4">
-                    <button className="active:bg-textmute md:hover:bg-textmute bg-text text-bg flex w-40 items-center justify-center gap-2 rounded-lg p-2">
-                        <PlusIcon className="h-5" />
-                        <p className="select-none">New Meeting</p>
+                <div
+                    className={`border-border flex w-full flex-col items-center gap-4 border-t py-10`}
+                >
+                    {user?.role === "admin" && (
+                        <button
+                            onClick={() => navigate("/admin/new-meeting")}
+                            className="active:bg-textmute md:hover:bg-textmute bg-text text-bg flex w-40 items-center justify-center gap-2 rounded-lg p-2"
+                        >
+                            <PlusIcon className="h-5" />
+                            <p className="select-none">New Meeting</p>
+                        </button>
+                    )}
+
+                    <button className="active:bg-bookedBg md:hover:bg-bookedBg text-bookedText border-bookedBorder flex w-40 items-center justify-center gap-2 rounded-lg border p-2">
+                        <LogoutIcon className="h-5" />
+                        <p className="select-none">Log Out</p>
                     </button>
                 </div>
             </div>

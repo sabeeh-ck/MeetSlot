739da5851f975237fd3f016c4e5c74ab1049423d
sabeeh <sabeehck02@gmail.com>
2026-08-11 21:31:15 +0530

    refactor: streamline MyMeetingsPage layout and enhance button styling for better responsiveness

--- client/src/pages/MyMeetingsPage.jsx
+++ client/src/pages/MyMeetingsPage.jsx
@@ -16,89 +16,42 @@ const MyMeetingsPage = () => {
     const upcoming = bookings.filter((b) => new Date(b.end) > now);
     const past = bookings.filter((b) => new Date(b.end) <= now);
 
-    const SkeletonLoad = () => (
-        <div>
-            <Skeleton
-                className="mb-2"
-                height={20}
-                width={120}
-                borderRadius={10}
-            />
-            <div className="flex flex-col gap-4 md:grid md:grid-cols-2">
-                <Skeleton height={isLaptop ? 132 : 112} borderRadius={12} />
-                <Skeleton height={isLaptop ? 132 : 112} borderRadius={12} />
-                {isTablet && (
-                    <Skeleton height={isLaptop ? 132 : 112} borderRadius={12} />
-                )}
-            </div>
-            <Skeleton
-                className="mt-6 mb-2"
-                height={20}
-                width={120}
-                borderRadius={10}
-            />
-            <div className="flex flex-col gap-4 md:grid md:grid-cols-2">
-                <Skeleton height={isLaptop ? 132 : 112} borderRadius={12} />
-                <Skeleton height={isLaptop ? 132 : 112} borderRadius={12} />
-            </div>
-        </div>
-    );
-
     return (
-        <div className="my-4 flex flex-1 flex-col">
-            <section className="flex flex-1 flex-col gap-6">
-                <div className="">
-                    <h2>My meetings</h2>
+        <section className="relative flex w-full flex-1 flex-col lg:flex-row lg:gap-0">
+            <div className="lg:border-border bg-bg fixed flex h-32 w-full flex-col gap-5 pb-4 lg:h-full lg:w-64 lg:border-r lg:p-0">
+                <div className="mx-4 mt-4 flex items-center justify-between">
+                    <h2 className="font-semibold">My Meetings</h2>
                 </div>
 
-                <div className="border-border bg-surface flex gap-1 rounded-xl border p-1">
+                <div className="border-border bg-surface lg:bg-bg mx-4 flex gap-1 rounded-xl border p-1 lg:flex-col lg:gap-2 lg:border-0 lg:p-0">
                     <button
                         onClick={() => setView("upcoming")}
-                        className={`flex flex-1 items-center justify-center gap-2 rounded-lg py-2 text-sm font-medium transition-all ${view === "upcoming" ? "bg-text text-bg shadow" : "text-textmute active:bg-border md:hover:bg-border"}`}
+                        className={`flex flex-1 items-center justify-center gap-1 rounded-lg py-2 text-sm font-medium transition-all lg:justify-between lg:px-2 ${view === "upcoming" ? "bg-text text-bg lg:text-text lg:bg-border shadow lg:shadow-none" : "lg:text-text text-textmute active:bg-border lg:hover:bg-border"}`}
                     >
-                        Upcoming{" "}
-                        {loading ? (
-                            <Skeleton
-                                width={16}
-                                height={20}
-                                containerClassName="leading-none"
-                                borderRadius={8}
-                                baseColor="#E0E0E0"
-                                highlightColor="#F5F5F5"
-                            />
-                        ) : (
-                            `(${upcoming.length})`
-                        )}
+                        <span>Upcoming</span>
+                        <span className="lg:hidden">-</span>
+                        <span>{upcoming.length}</span>
                     </button>
+
                     <button
                         onClick={() => setView("past")}
-                        className={`flex flex-1 items-center justify-center gap-2 rounded-lg py-2 text-sm font-medium transition-all ${view === "past" ? "bg-text text-bg shadow" : "active:bg-border text-textmute md:hover:bg-border"}`}
+                        className={`flex flex-1 items-center justify-center gap-1 rounded-lg py-2 text-sm font-medium transition-all lg:justify-between lg:px-2 ${view === "past" ? "bg-text text-bg lg:text-text lg:bg-border shadow lg:shadow-none" : "lg:text-text active:bg-border text-textmute lg:hover:bg-border"}`}
                     >
-                        Past{" "}
-                        {loading ? (
-                            <Skeleton
-                                width={16}
-                                height={20}
-                                containerClassName="leading-none"
-                                borderRadius={8}
-                            />
-                        ) : (
-                            `(${past.length})`
-                        )}
+                        <span>Past</span>
+                        <span className="lg:hidden">-</span>
+                        <span>{past.length}</span>
                     </button>
                 </div>
+            </div>
 
-                {loading ? (
-                    <SkeletonLoad />
-                ) : (
-                    <BookingsList
-                        bookings={view === "upcoming" ? upcoming : past}
-                        isPast={view === "past"}
-                        refetch={refetch}
-                    />
-                )}
-            </section>
-        </div>
+            <div className="from-bg via-bg/25 pointer-events-none fixed top-48 left-0 z-40 h-8 w-full bg-linear-to-b to-transparent lg:hidden" />
+
+            <BookingsList
+                bookings={view === "upcoming" ? upcoming : past}
+                isPast={view === "past"}
+                refetch={refetch}
+            />
+        </section>
     );
 };
 
